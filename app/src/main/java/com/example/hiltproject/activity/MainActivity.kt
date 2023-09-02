c5cdb0caaff39fef2627d2dbc70dcb47548aa033
Dorsa <dorsa.macky@gmail.com>
2023-09-02 14:03:55 +0430

    Fix manifest and MainActivity bugs

--- app/src/main/java/com/example/hiltproject/activity/MainActivity.kt
+++ app/src/main/java/com/example/hiltproject/activity/MainActivity.kt
@@ -3,12 +3,26 @@ package com.example.hiltproject.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.hiltproject.R
+import com.example.hiltproject.network.LivePriceApi
+import com.example.hiltproject.network.Repository
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    @Inject
+    lateinit var repository:Repository
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        CoroutineScope(Dispatchers.Main).launch {
+
+            repository.call()
+        }
     }
 }
--- app/src/main/java/com/example/hiltproject/network/Repository.kt
+++ app/src/main/java/com/example/hiltproject/network/Repository.kt
@@ -2,22 +2,22 @@ package com.example.hiltproject.network
 
 import android.util.Log
 import com.example.hiltproject.model.DigitalCoin
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 class Repository @Inject constructor(val api: LivePriceApi) {
-    lateinit var response:List<DigitalCoin>
-    fun call(){
-        CoroutineScope(Dispatchers.Main).launch {
-            response=api.getCoins(
+    private var response: List<DigitalCoin>? = null // Use nullable type to indicate it's not initialized
+
+    suspend fun call() {
+        try {
+            response = api.getCoins(
                 pageSize = 10,
                 pageIndex = 0,
                 type = 1
             )
+            Log.d("response", response?.get(0).toString()) // Access response here
+        } catch (e: Exception) {
+            // Handle exceptions
+            Log.e("error", e.message ?: "Unknown error")
         }
-
-        Log.d("response",response.get(0).toString())
     }
 }
