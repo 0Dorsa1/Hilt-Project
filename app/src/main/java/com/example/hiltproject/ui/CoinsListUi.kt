aa6a7cd86af841999db70d091b4240796a2d474b
Dorsa <dorsa.macky@gmail.com>
2023-09-03 10:40:32 +0430

    Latest Version

--- app/src/main/java/com/example/hiltproject/ui/CoinsListUi.kt
+++ app/src/main/java/com/example/hiltproject/ui/CoinsListUi.kt
@@ -1,15 +1,12 @@
 package com.example.hiltproject.ui
 
-import android.app.Application
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.hiltproject.di.RetrofitModule
 import com.example.hiltproject.model.DigitalCoin
 import com.example.hiltproject.viewmodel.CoinsViewModel
 
